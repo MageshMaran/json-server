6b1e0b252945fdc27173fde7a78165ac72e37c5b
MageshMaran <sujinmagesh@gmail.com>
2024-07-17 16:07:40 +0530

    Create hbdslider.js

---
+++ hbdslider.js
@@ -0,0 +1,22 @@
+{
+  "widgetTitle_1": "Categories",
+  "widgetTitle_2": "Popular Tags",
+  "widgetTitle_3": "Share This",
+  "widgetData_1": [
+    {
+      "id": 1,
+      "title": "Metaverse",
+      "content": "23"
+    },
+    {
+      "id": 2,
+      "title": "IGO",
+      "content": "15"
+    },
+    {
+      "id": 3,
+      "title": "Token",
+      "content": "12"
+    }
+  ]
+}
